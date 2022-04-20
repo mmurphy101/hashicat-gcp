a11db6af79e84e5254770b300cdab37653afea14
mmurphy101 <mmurphy101@partners.org>
2022-04-20 18:24:33 +0000

    Added VPC module3

--- vpc.tf
+++ vpc.tf
@@ -2,13 +2,13 @@ module "network" {
   source  = "app.terraform.io/DarkOrg/network/google"
   version = "3.4.0"
   # insert required variables here
-
-  subnets = [
-  {
-    subnet_name   = "gaurav-subnet"
-    subnet_ip     = "10.100.10.0/24"
-    subnet_region = var.region
+network_name = "marc"
+project_id = "10"
+    subnets = [
+    {
+        subnet_name   = "gaurav-subnet"
+        subnet_ip     = "10.100.10.0/24"
+        subnet_region = var.region
   }
 ]
 }
-
